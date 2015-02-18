b52f1c7e6b4081b6ed1fb49d8a2fb9f07e67655a
あまみや ゆうこ <1@4321.io>
2015-02-18 13:52:11 +0800

    33

--- YuukoOfficialSite/Models/YuukoContext.cs
+++ YuukoOfficialSite/Models/YuukoContext.cs
@@ -9,14 +9,10 @@ namespace YuukoOfficialSite.Models
     public class YuukoContext : DbContext
     {
         public DbSet<Document> Documents { get; set; }
-
         public DbSet<Sample> Samples { get; set; }
-
         public DbSet<Log> Logs { get; set; }
-
         public YuukoContext() : base("mssqldb")
         {
-
         }
     }
 }
